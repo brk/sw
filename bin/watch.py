6390e705f9a567ac204aed7cd1eed4ae3e013bd8
ben <ben@localhost>
2009-09-27 05:17:07 +0000

    Check for maxfiles in watch.py

--- bin/watch.py
+++ bin/watch.py
@@ -54,6 +54,9 @@ def main():
     print usage()
     sys.exit(1)
 
+ if len(files) > maxfiles:
+   print "%(watch)s: too many files to watch" % sys.argv[0]
+
   mtimes = dict([(f, try_get_mtime(f)) for f in files])
   done = False
   while not done:
